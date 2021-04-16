c37065688d9cd3a73ce5efdea56cf9f4f3a5d0c9
antoniobarolo <antoniobarolog@gmail.com>
2021-04-16 20:36:30 -0300

    tests

--- views/cronometro.js
+++ views/cronometro.js
@@ -1,1 +1,13 @@
 let tempo = 0
+
+function Iniciar(duracao) {
+    tempo = duracao
+    setInterval(function () {
+        tempo--;
+        $("#tempo").text(tempo)
+        if (tempo == 0) {
+            $("body").css("background-color", "lightgray")
+            tempo = 5
+        }
+    }, 1000)
+}
--- views/script.js
+++ views/script.js
@@ -1,19 +1,21 @@
 var palavra = $(".palavra").text()
 var pontos = 0
 
-function validarPalavra(){
+$("#botao-validar").on("click", function ValidarPalavra(){
     if (palavra == $("#palpite").val()){
         $("#resposta").text("Correto!")
         pontos++
-        setPontos()
+        SetPontos()
     }
     else{
         $("#resposta").text("Falso!")
         pontos--
-        setPontos()
+        SetPontos()
     }
-}
+})
 
-function setPontos(){
+$("#palpite").on("input", function (){$("#resposta").text("Hmm...")})
+
+function SetPontos(){
     parseInt($("#pontos").text(pontos))
 }
