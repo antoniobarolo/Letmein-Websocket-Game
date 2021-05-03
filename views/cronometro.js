fbfab5feda2182f91c2bf173de024d514b3c8958
antoniobarolo <antoniobarolog@gmail.com>
2021-05-02 23:15:24 -0300

    commit

--- views/cronometro.js
+++ views/cronometro.js
@@ -6,8 +6,13 @@ function Iniciar(duracao) {
         tempo--;
         $("#tempo").text(tempo)
         if (tempo == 0) {
+            FinalizarCronometro()
             $("body").css("background-color", "lightgray")
             tempo = 5
         }
     }, 1000)
+}
+
+function FinalizarCronometro(){
+    
 }
